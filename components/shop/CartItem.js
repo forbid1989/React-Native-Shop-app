b88801f1e9f415728fa1ee7df9e329261433317c
Brean Alvarez <alvarez.allanbrean@bdo.com.ph>
2021-01-29 17:20:35 +0800

    Updated CartItem

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -3,14 +3,18 @@ import { View, Text, StyleSheet, TouchableOpacity, Platform} from 'react-native'
 import {Ionicons} from '@expo/vector-icons';
 
 const CartItem = props => {
-    return <View>
-        <Text>
-            <Text>QTY</Text><Text>TITLE</Text>
+    return <View style={styles.cartItem}>
+        <Text style={styles.itemData}>
+            <Text style={styles.quantity}>QTY</Text><Text style={styles.title}>TITLE</Text>
         </Text>
         <View>
             <Text>$AMT</Text>
-            <TouchableOpacity>
-                <Ionicons name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} />
+            <TouchableOpacity onPress={props.onRemove} style={styles.deleteButton}>
+                <Ionicons 
+                    name={Platform.OS === 'android' ? 'md-trash' : 'ios-trash'} 
+                    size={23}
+                    color="red"
+                />
             </TouchableOpacity>
         </View>
     </View>
